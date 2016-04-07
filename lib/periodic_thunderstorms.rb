290d9aaaf36ee3f5628c93759264bf2bc4b88bdb
Jeff Sandberg <paradox460@gmail.com>
2016-04-07 13:52:25 -0700

    Fix up core of client

--- lib/periodic_thunderstorms.rb
+++ lib/periodic_thunderstorms.rb
@@ -1,7 +1,20 @@
-require "periodic_thunderstorms/version"
+require 'bundler/setup'
+Bundler.setup(:default)
 
+require 'require_all'
+
+require 'http_api_builder'
+
+require 'periodic_thunderstorms/version'
+require 'periodic_thunderstorms/http_client'
+
+require_rel 'periodic_thunderstorms/components/**/*.rb'
+
+require 'periodic_thunderstorms/client'
+
+# Periodic Thunderstorms is a utility wrapper for the CloudElements APi
 module PeriodicThunderstorms
-  def self.client
-    Client.new
+  def self.client(auth)
+    Client.new(auth)
   end
 end
--- lib/periodic_thunderstorms/client.rb
+++ lib/periodic_thunderstorms/client.rb
@@ -1,10 +1,23 @@
 module PeriodicThunderstorms
-  class Client < HttpApiBuilder::BaseClient
-    include PeriodicThunderstorms::HttpClient
+  # CloudElements client
+  class Client < ::HttpApiBuilder::BaseClient
+    include PeriodicThunderstorms::MockHttpClient
+
+    attr_accessor :auth
 
     base_url 'https://api.cloud-elements.com/elements/api-v2'
 
     @pagination = %i(nextPage pageSize)
 
+    # Platform includes
+    %w(Accounts ElementsBuilder Formulas Hubs Instances Organizations Usage Users).each do |const|
+      include Object.const_get("PeriodicThunderstorms::Components::Platform::#{const}")
+    end
+
+    include PeriodicThunderstorms::Components::Elements
+
+    def initialize(auth)
+      @auth = auth
+    end
   end
 end
