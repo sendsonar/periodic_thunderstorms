f9a2e0f6061f6f1d13e257d3a8bb025997c6d5c4
Jeff Sandberg <paradox460@gmail.com>
2016-04-12 12:34:44 -0700

    Fix http_client to work with CE

--- lib/periodic_thunderstorms/http_client.rb
+++ lib/periodic_thunderstorms/http_client.rb
@@ -4,10 +4,8 @@ require 'pry'
 module PeriodicThunderstorms
   # HTTP client implementing CloudElements auth
   module HttpClient
-    def request(verb, path, form:, query:, body:, json:) # rubocop:disable Metrics/ParameterLists
-      url = URI.join(self.class.base_url || '', path)
-      binding.pry
-      HTTP.auth(@auth).send(verb, url, form: form, params: query, body: body, json: json)
+    def request(verb, uri, form:, query:, body:, json:) # rubocop:disable Metrics/ParameterLists
+      HTTP.auth(@auth).accept('application/json').headers('Content-Type' => 'application/json').request(verb, uri, form: form, params: query, body: body, json: json)
     end
   end
 end
